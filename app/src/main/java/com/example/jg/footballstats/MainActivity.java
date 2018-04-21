250f78eadf239f110c53c46aff431d4547d5681b
Georgiy Kartashov <georgij.kartashov@yandex.ru>
2018-04-21 03:07:00 +0300

    Navigation drawer and toolbar minor fixes Added fragments as navigation drawer items

--- app/src/main/java/com/example/jg/footballstats/MainActivity.java
+++ app/src/main/java/com/example/jg/footballstats/MainActivity.java
@@ -2,8 +2,9 @@ package com.example.jg.footballstats;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -26,26 +27,39 @@ public class MainActivity extends AppCompatActivity {
         navigationView = findViewById(R.id.nav_view);
 
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_launcher_foreground);
+
+        ActionBarDrawerToggle menuToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.app_name, R.string.app_name);
+        drawerLayout.addDrawerListener(menuToggle);
+        menuToggle.syncState();
 
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                Class fragmentClass;
+
+                switch (item.getItemId()) {
+                    case R.id.nav_camera:
+                        fragmentClass = FirstFragment.class;
+                        try {
+                            fragment = (Fragment) fragmentClass.newInstance();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_layout, fragment,"sraka").commit();
+                        break;
+                    default:
+                        fragment = getSupportFragmentManager().findFragmentByTag("sraka");
+                        if (fragment != null)
+                            getSupportFragmentManager().beginTransaction().remove(fragment).commit();
+                }
+
                 item.setChecked(true);
-                drawerLayout.closeDrawers();
                 getSupportActionBar().setTitle(item.getTitle());
+                drawerLayout.closeDrawers();
+
                 return true;
             }
         });
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                drawerLayout.openDrawer(android.support.v4.view.GravityCompat.START);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
