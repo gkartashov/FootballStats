ff0e37a9681e7b60680f685422e6226f58e12a46
Georgiy Kartashov <georgij.kartashov@yandex.ru>
2018-04-22 09:59:23 +0300

    Added Splash Screen

---
+++ app/src/main/java/com/example/jg/footballstats/LaunchActivity.java
@@ -0,0 +1,27 @@
+package com.example.jg.footballstats;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme_LauncherTheme);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+
+        final Intent intent = new Intent(this, MainActivity.class);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+
+    }
+}
